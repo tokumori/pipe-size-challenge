90e4192cec5f2ab687b91fbb6d986e9b2b3e4798
Bryce <bsaito88@gmail.com>
2017-03-03 01:05:59 -1000

    Refacted Z parts.
    - Now pushes the sum into the argument array.
    - Conditional will only push pipeSize for the last item in the array.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function minPipeSize (rateArray) {
   if (Array.isArray(rateArray) && rateArray.length === 2) {
+    let rateZ = rateArray[0] + rateArray[1];
+    rateArray.push(rateZ)
     let tolerance = 2/100;
     let sizeArray = [];
     let pipeSizes = [8,10,12,16,20,25,32,40,50,63,80,100,127,160,201,254,320,404,509,642,810,1021,1288,1624,2048,2582,3256,4106,5178,6529,8233,10382,13091,16507,20815,26248,33098,41735,52627,66361,83680,105518,133056,167780,211566,250000,300000,350000,400000,450000,500000,600000,700000,750000,800000,900000,1000000,1250000,1500000,1750000,2000000];
@@ -11,36 +13,26 @@ function minPipeSize (rateArray) {
       let rate = rateArray[i];
       let minPipeSize = (1.732 * 12.9 * 200 * rate)/ (208 * tolerance);
       let minValveSize = rate * 1.25 * 1.25;
+
+      // tried to do for loop, but reduce was faster to code.
+      // might change after completing challenge.
       let pipeSize = pipeSizes.reduce((accumulator, currentVal) => {
         return accumulator > minPipeSize ? accumulator : currentVal;
       });
-      let valveSize = valveSizes.reduce((accumulator, currentVal) => {
-        return accumulator > minValveSize ? accumulator : currentVal;
-      });
-      sizeArray.push(pipeSize, valveSize);
+      if (rateArray[i] !== rateArray[rateArray.length - 1]) {
+        let valveSize = valveSizes.reduce((accumulator, currentVal) => {
+          return accumulator > minValveSize ? accumulator : currentVal;
+        });
+        sizeArray.push(pipeSize, valveSize);
+      } else {
+        sizeArray.push(pipeSize);
+      }
     }
-    let rateX = rateArray[0];
-    let rateY = rateArray[1];
-    let rateZ = rateArray[0] + rateArray[1];
-    let minPipeSizeZ = (1.732 * 12.9 * 200 * rateZ)/ (208 * tolerance);
-    let pipeSizeZ = pipeSizes.reduce((accumulator, currentVal) => {
-        return accumulator > minPipeSizeZ ? accumulator : currentVal;
-    });
-    // let minPipeSizeX = (1.732 * 12.9 * 200 * rateX)/ (208 * tolerance);
-    // let minValveSizeX = rateX * 1.25 * 1.25;
-    // let minPipeSizeY = (1.732 * 12.9 * 200 * rateY)/ (208 * tolerance);
-    // let minValveSizeY = rateY * 1.25 * 1.25;
-
-    // tried to do for loop, but reduce was faster to code.
-    // might change after completing challenge.
-
-    // let pipeSizeY = pipeSizes.reduce((accumulator, currentVal) => {
-    //   return accumulator > minPipeSizeY ? accumulator : currentVal;
+    // let minPipeSizeZ = (1.732 * 12.9 * 200 * rateZ)/ (208 * tolerance);
+    // let pipeSizeZ = pipeSizes.reduce((accumulator, currentVal) => {
+    //     return accumulator > minPipeSizeZ ? accumulator : currentVal;
     // });
-    // let valveSizeY = valveSizes.reduce((accumulator, currentVal) => {
-    //   return accumulator > minValveSizeY ? accumulator : currentVal;
-    // });
-    sizeArray.push(pipeSizeZ);
+    // sizeArray.push(pipeSizeZ);
     return console.log(sizeArray);
   }
   return console.log('Please enter an array with only two numbers.');
